2672e00fe05593103f51983b5e17b59c221588a1
Nicole Freed <nicole.freed@gmail.com>
2017-08-04 09:38:41 -0700

    create DoctorLookup object declaration w/getDoctors prototype method (API call)

--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,6 +1,10 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getDoctors = function(medicalIssue) {
+function DoctorLookup() {
+
+}
+
+DoctorLookup.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
@@ -9,3 +13,5 @@ exports.getDoctors = function(medicalIssue) {
       console.log("fail");
     });
 };
+
+exports.doctorLookupModule = DoctorLookup;
