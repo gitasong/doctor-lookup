d6fcf76aa46a7ddd8276509f4768198100ca7119
Nicole Freed <nicole.freed@gmail.com>
2017-08-04 10:08:59 -0700

    create doctor-lookup-interface.js with form handler, new object instantiation, and call to api function

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,1 +1,12 @@
-var apiKey = require('./../.env').apiKey;
+// var apiKey = require('./../.env').apiKey;
+var DoctorLookup = require('./../js/doctor-lookup.js').doctorLookupModule;
+
+$(document).ready(function() {
+  $("#medical-issue-submit").click(function() {
+    var medicalIssue = $("#medical-issue").val();
+    $("#medical-issue").val("");
+    var newDoctorLookup = new DoctorLookup();
+    var doctors = newDoctorLookup.getDoctors(medicalIssue);
+    // $("#show-doctors").text(result.data.practices.name);
+  });
+});
