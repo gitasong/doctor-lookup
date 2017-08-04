098434e813713cfb64aa3cc93ee9b6beb58b93fb
Nicole Freed <nicole.freed@gmail.com>
2017-08-04 13:36:53 -0700

    tried to get promises, asynchrony, and callback functions to work; couldn't

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,12 +1,19 @@
 // var apiKey = require('./../.env').apiKey;
 var DoctorLookup = require('./../js/doctor-lookup.js').doctorLookupModule;
 
+var displayRawData = function(result) {
+  // console.log(result);
+  $('#show-doctors').text(result);
+};
+
 $(document).ready(function() {
   $("#medical-issue-submit").click(function() {
     var medicalIssue = $("#medical-issue").val();
     $("#medical-issue").val("");
-    var newDoctorLookup = new DoctorLookup();
-    var doctors = newDoctorLookup.getDoctors(medicalIssue);
-    // $("#show-doctors").text(result.data.practices.name);
+    var newDoctorLookup = new DoctorLookup(medicalIssue);
+    console.log(newDoctorLookup);
+    var doctors = newDoctorLookup.getDoctors(medicalIssue);  // need to format
+    console.log(doctors);
+    // $("#show-doctors").text(doctors);
   });
 });
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -1,17 +1,48 @@
 var apiKey = require('./../.env').apiKey;
 
-function DoctorLookup() {
+function DoctorLookup(medicalIssue) {
+  this.medicalIssue = medicalIssue;
+}
 
+function parseDoctorsData(result) {
+  var doctorsArray = [];
+  for (i = 0; i < result.data.length; i++) {
+    for (j = 0; j < result.data.practices.length; j++) {
+      doctorsArray.push(result.data[i].practices[j].name);
+      // console.log(doctorsArray);
+    }
+  }
+  return doctorsArray;
 }
 
 DoctorLookup.prototype.getDoctors = function(medicalIssue) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
+    .then(function(result) {
+      // $("show-doctors").text("Your results: " + result);
+    //   // medicalIssue = this.medicalIssue;
+    //   // $('.showDoctors').text(result.JSON.stringify);
       console.log(result);
+      return result;
+      // var doctorsData = displayResult(result);
+      // console.log(doctorsData);
+    //   // return doctorsArray;
+    //   // var doctorsData = displayDoctors(result);  // result.data[i].practices[j].name
+    //   // console.log(doctorsData);
     })
-   .fail(function(error){
+    .fail(function(error){
       console.log("fail");
     });
+};
+
+DoctorLookup.prototype.doctorData = function(displayRawData) {
+  var response = this.apiCall;
+  response.then(function(response) {
+    console.log(response);
+    displayRawData(response);
+  })
+  .fail(function(error) {
+    console.log("fail");
+  });
 };
 
 exports.doctorLookupModule = DoctorLookup;
