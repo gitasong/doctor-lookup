d29338b72c3411ecb659e40de579b482f68827e6
Nicole Freed <nicole.freed@gmail.com>
2017-08-06 17:12:26 -0700

    create getSpecialties() prototype method with API call and promise

--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -4,6 +4,19 @@ function DoctorLookup(medicalIssue) {
   this.medicalIssue = medicalIssue;
 }
 
+DoctorLookup.prototype.getSpecialties = function(displaySpecialties) {
+  $.get('https://api.betterdoctor.com/2016-03-01/specialties?skip=0&limit=20&user_key=' + apiKey)
+    .then(function(result) {
+      var specialtiesArray = [];
+      for (i = 0; i < result.data.length; i++) {
+        specialtiesArray.push(result.data[i].name);
+      }
+    })
+    .fail(function(error) {
+      console.log("fail");
+    });
+};
+
 DoctorLookup.prototype.getDoctors = function(medicalIssue, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
     .then(function(result) {
